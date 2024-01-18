d262740b251a803693ed9522a93d278a9db17584
Yoann Cribier <yoann_cribier@hotmail.com>
2024-01-18 15:38:49 +0300

    test updated

--- src/test.ts
+++ src/test.ts
@@ -1,42 +1,24 @@
 import "dotenv/config";
+import { readFileSync } from 'fs';
 import { Client, User } from "./index";
 
-const isPisciner = (user: User) =>
-  user.pool_month === "august" && user.pool_year === "2023";
-(async () => {
-<<<<<<< HEAD
-	const client = new Client(
-		<string>process.env.ID,
-		<string>process.env.SECRET
-	);
+const path = process.argv[2];
+const examId = Number(process.argv[3]);
 
-	const auth_process = await client.auth_manager.init_auth_process(
-		"http://localhost:3333/callback",
-		["public", "projects", "profile"],
-		{
-			port: 3333,
-			callback_function: async (user) => {
-				console.log(`Welcome ${user.login}!`);
-				if (auth_process)
-					client.auth_manager.stop_auth_process(auth_process.id);
-			},
-		}
-	);
-	if (auth_process) console.log(auth_process.url);
-=======
+const examMasterUsersLogins = readFileSync(path).toString().split("\n");
+
+const main = async () => {
   const client = new Client(
     <string> process.env.ID,
     <string> process.env.SECRET,
   );
 
-  const eventUsers = await client.events_users.fetch(18260);
-  const pisciners = eventUsers.filter((eu) => isPisciner(eu.user));
-  console.log(`
-    total: ${eventUsers.length}
-    Pisciners: ${pisciners.length}
-    Studs: ${eventUsers.length - pisciners.length}
-    `);
-  const studs = eventUsers.filter((eu) => !isPisciner(eu.user));
-  console.log(studs.map((s) => s.user.login).join("\n"));
->>>>>>> b7672a5 (get studs and pisciners from an event)
-})();
+	const examEventUsers = await client.exams_users.fetch(examId);
+	const examEventUsersLogins = examEventUsers.map(s => s.user.login);
+
+	const res = examEventUsers
+		.filter(s => !examMasterUsersLogins.includes(s.user.login))
+	console.log(res.map(s => [s.id, s.user.login, s.user.id]));
+}
+
+main()
